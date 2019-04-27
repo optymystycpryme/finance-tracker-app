43035fae784d5067064c6d13bdd48fdd654efb76
brian haltom <brianwh87@gmail.com>
2019-04-27 10:52:36 -0700

    Integrate friendship controller, users show page, friends page, and styling

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,7 @@
 class StocksController < ApplicationController
   def search
     if params[:stock].blank?
-      flash.now[:danger] = 'Search can not be empty'
+      flash.now[:danger] = 'Search cannot be empty'
     else
       @stock = Stock.new_from_lookup(params[:stock])
       flash.now[:danger] = 'You have entered an invalid entry' unless @stock
